ae6b2c41adfaacff916bce82295b609718a55b77
haibo888 <392747379@qq.com>
2020-03-31 14:23:53 +0800

    第一次修改

--- src/main/java/com/controller/Test1.java
+++ src/main/java/com/controller/Test1.java
@@ -9,6 +9,6 @@ import java.util.HashMap;
 public class Test1 {
     public static void main(String[] args) {
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
-
+        System.out.println("提交测试");
     }
 }
